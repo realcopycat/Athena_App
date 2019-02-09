380f53d076b6445057c30210a95f53cbf38bf0df
realcopycat <44093113+realcopycat@users.noreply.github.com>
2019-02-09 09:22:50 +0800

    requirement生成

--- athena_App/athena_App/views.py
+++ athena_App/athena_App/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, redirect, url_for
 from athena_App import app
 from athena_App.formClass import QuestionForm
 
@@ -14,10 +14,11 @@ def home():
     question = ''
     form = QuestionForm()
     question = form.question.data
-
+    if form.validate_on_submit():
+        return redirect(url_for('answer'))
     return render_template(
         'index.html',
-        title = 'Home Page',
+        title = 'Index Page',
         year = datetime.now().year,
         form =  form,
         question = question
@@ -42,3 +43,11 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/answer')
+def answer():
+    """Renders the answer page"""
+    return render_template(
+        'answer.html',
+        title='Answer'
+        )
