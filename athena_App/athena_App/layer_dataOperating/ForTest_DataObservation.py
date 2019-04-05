0b3c13abd77c609818d3e36d2eafa14858bdca76
realcopycat <44093113+realcopycat@users.noreply.github.com>
2019-04-05 15:17:00 +0800

    update

--- athena_App/athena_App/layer_dataOperating/ForTest_DataObservation.py
+++ athena_App/athena_App/layer_dataOperating/ForTest_DataObservation.py
@@ -1,1 +1,13 @@
+#探索mongodb数据
 
+from pymongo import MongoClient as MG
+
+client=MG()
+db=client['spider_data']
+collect=db.lawText
+
+data=collect.find()
+
+for item in data:
+    print(item['judgement']['plaintext'])
+    input()
